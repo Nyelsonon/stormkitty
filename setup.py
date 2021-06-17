00c9d41d918bc70bcc2993d35826c93b02ccae3c
Nyelsonon <51932119+Nyelsonon@users.noreply.github.com>
2021-06-17 18:15:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'stormkitty',         # How you named your package folder (MyLib)
   packages = ['stormkitty'],   # Chose the same as "name"
-  version = '0.9',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'stormbound pypi',   # Give a short description about your library
   author = 'nyelson',                   # Type in your name

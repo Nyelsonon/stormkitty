d5563450d5f8684ad50c3c7bdd36f5c1d2c002a8
Nyelsonon <51932119+Nyelsonon@users.noreply.github.com>
2021-06-16 21:21:16 -0400

    Update stormkitty.py

--- stormkitty.py
+++ stormkitty.py
@@ -2,29 +2,31 @@
 import csv
 import random
 import os
+import requests
 
 #variables stored in a neat little dictionary
-portable_variables = {"max_deck_count":12, "begin_count":0}
+portable_variables = {"max_deck_count":12, "begin_count":0, "CSV_URL":r'https://www.dropbox.com/s/acfzhdsostyd08s/cards.csv?dl=1'}
 
 #firstly lets generate a random deck using faction as our first param
-def generate_random_deck():
+def generate_random_deck(faction):
     global portable_variables
+    CSV_URL = portable_variables["CSV_URL"]
     main_deck = []
     max_deck_count = portable_variables["max_deck_count"]
-    print(max_deck_count)
     begin_count = portable_variables["begin_count"]
     while begin_count != max_deck_count:
-        with open(r'cards.csv') as f:
-            reader = csv.reader(f)
-            chosen_row = random.choice(list(reader))
-            faction = str(chosen_row[3])
-            if faction == "shadowfen" or faction == "neutral":
-                print(faction)
+        with requests.Session() as s:
+            download = s.get(CSV_URL)
+            decoded_content = download.content.decode('utf-8')
+            cards_url = csv.reader(decoded_content.splitlines(), delimiter=',')
+            cards = list(cards_url)
+            chosen_row = random.choice(list(cards))
+            found_faction = str(chosen_row[3])
+            if found_faction == str(faction) or found_faction == "neutral":
                 main_deck.append(chosen_row[0])
                 begin_count += 1
                 print(main_deck)
             else:
-                pass
+                print("->", end="")
 
     return main_deck
-print(generate_random_deck())

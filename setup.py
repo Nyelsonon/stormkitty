aa2e5c354eb7e0f9065fe741d38d631087445581
Nyelsonon <51932119+Nyelsonon@users.noreply.github.com>
2021-06-21 12:44:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'stormkitty',         # How you named your package folder (MyLib)
   packages = ['stormkitty'],   # Chose the same as "name"
-  version = '1.1',      # Start with a small number and increase it with every change you make
+  version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'stormbound pypi',   # Give a short description about your library
   author = 'nyelson',                   # Type in your name
